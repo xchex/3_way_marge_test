01ecf44134807d8ea7ac08a54f0aa1c0f563252c
xingjian <xingjianche@outlook.com>
2018-03-25 11:28:36 +1300

    C

--- Test.java
+++ Test.java
@@ -1,9 +1,16 @@
 class Test {
-    private String name;
-    private int semester;
+    final private String name;
+    final private int sem;
+    final private int year;
 
-public void Test(String name , int semester) {
+    public void Test(String name , int sem, int year) {
         this.name = name;
-        this.semester = semester;
+        this.sem = sem;
+        this.year = year;
+        System.out.println("Test constructor");
+    }
+
+    protected onFinished() {
+        System.out.println("Celebrate semester" + sem)
     }
 }
