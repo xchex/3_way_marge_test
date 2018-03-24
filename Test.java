0501c4f6991481bd07156285dc027031646a2f2b
xingjian <xingjianche@outlook.com>
2018-03-25 10:52:53 +1300

    First

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test {
+    private String name;
+    private int sem;
+
+public void Test(String name , int sem) {
+    this.name = name;
+    this.sem = sem;
+    System.out.println("Test constructor");
+    }
+}
