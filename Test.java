7ea136a0422287fdfb0e3179da3e9c17a08feada
xingjian <xingjianche@outlook.com>
2018-03-25 14:34:39 +1300

    2

--- Test.java
+++ Test.java
@@ -1,9 +1,10 @@
 class Test {
     private String name;
-    private int semester;
+    private int sem;
 
-public void Test(String name , int semester) {
+public void Test(String name , int sem) {
         this.name = name;
-        this.semester = semester;
+        this.sem = sem;
+        System.out.println("Test constructor");
     }
 }
